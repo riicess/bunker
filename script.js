19e451ccc12c322a94889f50ae4f779e0a8f270d
riicess <150382838+riicess@users.noreply.github.com>
2025-03-16 02:29:29 +0300

    Menu change!

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuBtn = document.getElementById('menuBtn');
-    const menu = document.getElementById('menu');
-
-    menuBtn.addEventListener('click', () => {
-        menu.classList.toggle('open');
-    });
-
-    // Redirect to "home" if the current path is the root
-    if (window.location.pathname === '/' || window.location.pathname === '/home.html') {
-        window.location.replace('/home');
-    }
-});
+// No code needed in this file for now
