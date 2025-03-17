e2a4e33f639323f62d196ee4b63a623a663c4aca
riices <150382838+riicess@users.noreply.github.com>
2025-03-17 18:54:38 +0300

    new menu

--- script.js
+++ script.js
@@ -3,4 +3,22 @@ document.addEventListener('DOMContentLoaded', () => {
     if (window.location.pathname === '/' || window.location.pathname === '' || window.location.pathname === '/bunker' || window.location.pathname === '/bunker/') {
         window.location.replace('/bunker/home');
     }
+
+    const scrolldown = document.getElementById('scrolldown');
+    const hiddenMenu = document.getElementById('hiddenMenu');
+    let holdTimeout;
+
+    scrolldown.addEventListener('mousedown', () => {
+        holdTimeout = setTimeout(() => {
+            hiddenMenu.classList.add('active');
+        }, 1000); // 1 second hold to show the menu
+    });
+
+    scrolldown.addEventListener('mouseup', () => {
+        clearTimeout(holdTimeout);
+    });
+
+    scrolldown.addEventListener('mouseleave', () => {
+        clearTimeout(holdTimeout);
+    });
 });
