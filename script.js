840062925a5dd63646c4ba614b3dd7175eed76e2
riices <150382838+riicess@users.noreply.github.com>
2025-03-17 18:38:23 +0300

    fixlast/ihope/

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Redirect to "home" if the current path is the root
-    if (window.location.pathname === '/' || window.location.pathname === '') {
+    // Redirect to "home" if the current path is the root or the root of the "bunker" directory
+    if (window.location.pathname === '/' || window.location.pathname === '' || window.location.pathname === '/bunker' || window.location.pathname === '/bunker/') {
         window.location.replace('/bunker/home');
     }
 });
