971bd2f98ee9d1a8fe4dfdff89883cb19cd75694
riices <150382838+riicess@users.noreply.github.com>
2025-03-17 18:35:10 +0300

    fix2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Redirect to "home" if the current path is the root
-    if (window.location.pathname === '/' || window.location.pathname === '/index.html') {
-        window.location.replace('/home');
+    if (window.location.pathname === '/' || window.location.pathname === '') {
+        window.location.replace('/bunker/home');
     }
 });
