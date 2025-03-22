a0d7b8cb4b0efb2db47fa124c2198b268349b05d
riices <150382838+riicess@users.noreply.github.com>
2025-03-23 01:00:35 +0300

    menuuu

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Redirect to "home" if the current path is the root or the root of the "bunker" directory
-    if (window.location.pathname === '/' || window.location.pathname === '' || window.location.pathname === '/bunker' || window.location.pathname === '/bunker/') {
-        window.location.replace('/bunker/home');
-    }
-
     const menuButton = document.getElementById('menuButton');
     const hiddenMenu = document.getElementById('hiddenMenu');
 
-    menuButton.addEventListener('click', (event) => {
-        event.stopPropagation(); // Prevent click event from bubbling to document
+    menuButton.addEventListener('click', () => {
         hiddenMenu.classList.toggle('show');
     });
 
-    // Close the menu if clicking outside
+    // Close menu when clicking outside
     document.addEventListener('click', (event) => {
         if (!menuButton.contains(event.target) && !hiddenMenu.contains(event.target)) {
             hiddenMenu.classList.remove('show');
